9e97aabc4de15fd438daed27d2e7fa8ef2acdc8a
Abdalqader27 <abdalqader27.najjar@gmail.com>
2022-11-19 12:54:50 +0300

    update:add 8 kyu-Reversed Strings

--- problems/reversed-strings.dart
+++ problems/reversed-strings.dart
@@ -5,12 +5,11 @@ main() {
 }
 
 String solution(str) {
-
-  solve1(String str) {
+  String solve1(String str) {
     return str.split('').reversed.join();
   }
 
-  solve2(str) {
+  String solve2(str) {
     String res = "";
     for (int i = str.length - 1; i >= 0; i--) {
       res += str[i];
